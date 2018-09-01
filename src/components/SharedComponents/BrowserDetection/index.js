c46c14abf6eb9e7ea0b8766dab385f7812a178d3
James Ayres <jfrancis.ayres@gmail.com>
2018-08-31 22:10:49 -0400

    Browser Detect fixes

--- src/components/SharedComponents/BrowserDetection/index.js
+++ src/components/SharedComponents/BrowserDetection/index.js
@@ -7,7 +7,7 @@ const BrowserDetection = () => (
       <em>dead</em> is a P2P social network that is only supported on Beaker
       Browser. If you want to set up <em>dead</em>{' '}
       <a href="https://beakerbrowser.com/">Download Beaker Browser</a> and{' '}
-      <a href="https://github.com/dumblr/xXx___dead___xXx">
+      <a href="https://github.com/dumblr/dead-lite">
         read the directions for setting up <em>dead</em> on Github
       </a>.
     </p>
